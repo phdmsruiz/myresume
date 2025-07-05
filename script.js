1a7e100cd57f4551644a6106cce2a43130102b16
Melissa S. R. <melissa.salinas.ruiz@gmail.com>
2025-07-05 18:34:59 -0300

    corrigindo o javascript script.js

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ document.addEventListener("DOMContentLoaded", function() {
   alert("Bem-vindo ao site de Melissa Salinas Ruiz!");
 });
 
-const nome = prompt ("Qual é o seu nome?");
-document.getElementById("saudacao").textContent = 'Olá, ${nome}! Obrigada pela visita';
