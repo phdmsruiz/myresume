13df519d4ff9b3200725e1c98e71b7480bcc5029
Melissa S. R. <melissa.salinas.ruiz@gmail.com>
2025-07-05 18:48:26 -0300

    adicionando javascript do formulário script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+//adicionando mensagem de saudação
 document.addEventListener("DOMContentLoaded", function() {
   alert("Bem-vindo ao site de Melissa Salinas Ruiz!");
-});
 
+  const formulario = document.getElementById("formularioContato");
+
+//adicionando funcionalidades javascript para o formulário
+  formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede envio real
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    alert(`Obrigado, ${nome}!\nRecebemos sua mensagem:\n"${mensagem}"\nResponderemos em breve pelo e-mail: ${email}`);
+  });
+});
