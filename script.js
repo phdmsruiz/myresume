d81a04ae3929c7535f213b6d6fd1acb6eb8e5534
Melissa S. R. <melissa.salinas.ruiz@gmail.com>
2025-07-05 18:20:24 -0300

    Adicionando saudacao script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   alert("Bem-vindo ao site de Melissa Salinas Ruiz!");
 });
+
+const nome = prompt ("Qual é o seu nome?");
+document.getElementById("saudacao").textContent = 'Olá, ${nome}! Obrigada pela visita';
